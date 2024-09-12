8c78efb76713beea75c071fdb6d8ffb3d785c9f6
JHS <wkdgytmd200@naver.com>
2024-09-12 17:04:38 +0900

    Design : 하단 내비게이션 바 디자인
    * chore : svg 사용을 위한 라이브러리 추가 및 설정
    * design : 하단 내비게이션 바 디자인

---
+++ accountbook_front/src/screen/accountBook/calendar/CalendarScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+interface CalendarScreenProps {}
+
+function CalendarScreen({}: CalendarScreenProps) {
+  return (
+    <View>
+      <Text>테스트</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default CalendarScreen;
