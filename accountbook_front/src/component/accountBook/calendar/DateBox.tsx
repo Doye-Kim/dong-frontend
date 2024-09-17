fe402dc4f59e842e3b02d7cad35d7b8aef6f5ec5
JHS <wkdgytmd200@naver.com>
2024-09-17 15:45:59 +0900

    DESIGN : 달력 페이지 디자인 구현

---
+++ accountbook_front/src/component/accountBook/calendar/DateBox.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {StyleSheet, View, Text, Pressable, Dimensions} from 'react-native';
+import {colors} from '../../../constants';
+
+interface DateBoxProps {
+  date: number | null;
+  selectedDate: number;
+  onPressDate: (date: number) => void;
+  expense? :number;
+}
+
+const deviceWidth = Dimensions.get('window').width;
+
+const DateBox = ({date, selectedDate, onPressDate, expense}: DateBoxProps) => {
+
+  if (date === null) {
+    return <View style={styles.emptyBox}></View>
+  }
+
+  return (
+    <Pressable style={styles.container} onPress={() => onPressDate(date)}>
+      {date > 0 && (
+        <>
+          <View
+            style={[
+              styles.dateContainer,
+              selectedDate === date && styles.selectedContainer,
+            ]}>
+            <Text
+              style={[
+                styles.dateText,
+                selectedDate === date && styles.selectedDateText,
+              ]}>
+              {date}
+            </Text>
+            { expense && (
+              <Text style={styles.expenseText}>
+                {expense.toLocaleString()}원
+              </Text>
+            )
+
+            }
+          </View>
+        </>
+      )}
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: deviceWidth / 7,
+    height: deviceWidth / 7 + 20,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: colors.GRAY_200,
+    alignItems: 'center',
+  },
+  dateContainer: {
+    marginTop: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 28,
+    height: 28,
+    borderRadius: 28,
+  },
+  dateText: {
+    fontSize: 18,
+    color: colors.BLACK,
+    fontFamily: 'Pretendard-Bold'
+  },
+  selectedContainer: {
+    backgroundColor: colors.BLACK,
+    borderRadius: 30,
+  },
+  selectedDateText: {
+    color: colors.WHITE,
+    fontWeight: 'bold',
+  },
+  expenseText: {
+    fontSize: 12,
+    color: colors.BLACK,
+    marginTop: 4,
+  },
+  emptyBox: {
+    width: deviceWidth / 7,
+    height: deviceWidth / 7 + 20, 
+  },
+});
+
+export default DateBox;
--- accountbook_front/src/screen/accountBook/calendar/CalendarScreen.tsx
+++ accountbook_front/src/screen/accountBook/calendar/CalendarScreen.tsx
@@ -9,6 +9,12 @@ interface CalendarScreenProps {}
 function CalendarScreen({}: CalendarScreenProps) {
   const currentMonthYear = getMonthYearDetails(new Date());
   const [monthYear, setMonthYear] = useState(currentMonthYear);
+  const [selectedDate, setSelectedDate] = useState(0);
+  const [expenses, setExpenses] = useState([]);
+
+  const handlePressDate = (date: number) => {
+    setSelectedDate(date);
+  };
 
   const handleUpdateMonth = (increment: number) => {
     setMonthYear(prev => getNewMonthYear(prev, increment));
@@ -16,7 +22,13 @@ function CalendarScreen({}: CalendarScreenProps) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <CustomCalendar monthYear={monthYear} onChangeMonth={handleUpdateMonth}/>
+      <CustomCalendar
+        monthYear={monthYear}
+        onChangeMonth={handleUpdateMonth}
+        selectedDate={selectedDate}
+        onPressDate={handlePressDate}
+        expenses={expenses}
+      />
     </SafeAreaView>
   );
 }
