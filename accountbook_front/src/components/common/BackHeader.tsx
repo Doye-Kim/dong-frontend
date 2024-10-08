96bf1e929173c2d1f19fd55dfc9bcb52ca1a9768
김도예 <kmdy125@gmail.com>
2024-10-08 15:55:47 +0900

    Bug: 뒤로가기 에러 수정

--- accountbook_front/src/components/common/BackHeader.tsx
+++ accountbook_front/src/components/common/BackHeader.tsx
@@ -1,12 +1,20 @@
 import {BackArrow} from '@/assets/icons';
+import {accountBookNavigations, mainNavigations} from '@/constants';
 import {View, TouchableOpacity} from 'react-native';
 
 const BackHeader = ({navigation}) => {
+  const handleGoBackOrNavigate = () => {
+    if (navigation.canGoBack()) {
+      navigation.goBack();
+    } else {
+      navigation.navigate(mainNavigations.ACCOUNTBOOK, {
+        screen: accountBookNavigations.TABBAR,
+      });
+    }
+  };
   return (
     <View style={{flexDirection: 'row', alignItems: 'center'}}>
-      <TouchableOpacity
-        style={{margin: 10}}
-        onPress={() => navigation.goBack()}>
+      <TouchableOpacity style={{margin: 10}} onPress={handleGoBackOrNavigate}>
         <BackArrow width={30} height={30} />
       </TouchableOpacity>
     </View>
