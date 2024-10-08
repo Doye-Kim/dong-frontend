c8adb3d31c6453d3e7af705465828fc376ade463
장효승 <wkdgytmd200@naver.com>
2024-10-08 12:39:18 +0900

    Feat : 내역 페이지 수정

--- accountbook_front/src/components/accountBook/payment/PaymentItemList.tsx
+++ accountbook_front/src/components/accountBook/payment/PaymentItemList.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {StyleSheet, View, FlatList, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {colors} from '@/constants';
 import PaymentItem from './PaymentItem'; // 새로 만든 PaymentItem 컴포넌트
-import {formatDateToDayOfWeek} from '@/utils';
+import {formatDateToDayOfWeek, getDateWithSeparator} from '@/utils';
 import {Payment} from '@/types/domain';
 import {AccountBookStackParamList} from '@/navigations/stack/accountBook/AccountBookStackNavigator';
+import {RefreshControl} from 'react-native-gesture-handler';
+import usePaymentDataStore from '@/store/usePaymentDataStore';
+import useDateStore from '@/store/useDateStore';
 
 type GroupedPayments = Record<string, Payment[]>;
 
@@ -27,10 +30,7 @@ const groupPaymentsByDate = (payments: Payment[]) => {
   }, {} as GroupedPayments);
 };
 
-const PaymentItemList = ({
-  payments,
-  onPaymentPress,
-}: PaymentItemListProps) => {
+const PaymentItemList = ({payments, onPaymentPress}: PaymentItemListProps) => {
   const navigation =
     useNavigation<StackNavigationProp<AccountBookStackParamList>>();
   const groupedPayments: GroupedPayments = groupPaymentsByDate(payments);
@@ -39,6 +39,18 @@ const PaymentItemList = ({
       onPaymentPress(payments_id);
     }
   };
+
+  const fetchPaymentData = usePaymentDataStore(state => state.fetchPaymentData);
+  const date = useDateStore(state => state.date);
+
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    fetchPaymentData(getDateWithSeparator(date, '-'));
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 500);
+  }, [fetchPaymentData, date]);
 
   const renderGroup = ({item}: {item: string}) => {
     return (
@@ -57,6 +69,9 @@ const PaymentItemList = ({
 
   return (
     <FlatList
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
       data={Object.keys(groupedPayments)}
       renderItem={renderGroup}
       keyExtractor={item => item}
